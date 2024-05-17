d1b4dc87d89f1334f450ccf620db8d0526cde700
Gonzalo Perrone <gonza19913@gmail.com>
2024-05-16 22:33:14 -0300

    Agregando desafios 1 y 2 de tema 2.5

---
+++ tema2-5des1.py
@@ -0,0 +1,5 @@
+nombre = input("Ingrese su nombre: ")
+
+ciudad_de_origen = input("Ingrese su localiad de origen: ")
+
+print("Bienvenido " + nombre + " de " + ciudad_de_origen + " al CERP Suroeste")
---
+++ tema2-5des2.py
@@ -0,0 +1,19 @@
+numero1 = int(input("Ingrese el primer numero: "))
+numero2= int(input("Ingrese el segundo numero: "))
+
+def suma(numero1, numero2):
+    print("La suma es: " + str(numero1 + numero2))
+
+def resta(numero1, numero2):
+    print("La resta es: " + str(numero1 - numero2))
+    
+def multiplicacion(numero1, numero2):
+    print("El producto de la multiplicacion es: " + str(numero1 * numero2))
+    
+def division(numero1, numero2):
+    print("El producto de la division es: " + str(numero1 / numero2))
+    
+suma(numero1, numero2)
+resta(numero1, numero2)
+multiplicacion(numero1, numero2)
+division(numero1, numero2)
